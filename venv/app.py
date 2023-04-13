4dd1d478dd7a72f05f424eb4349cb22d2793e651
Eli Huffman <113002152+wolfyman411@users.noreply.github.com>
2023-04-12 22:27:38 -0400

    functional buttons?

--- venv/app.py
+++ venv/app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+app = Flask(__name__)
+
+@app.route('/')
+def index():  
+    return render_template('login.html')
+
+@app.get('/newAccount')
+def list_all_movies():
+    return render_template('newAccount.html')
+
+@app.get('/home')
+def list_all_movies():
+    return render_template('home.html')
+
+@app.get('/createpost')
+def list_all_movies():
+    return render_template('createpost.html')
+
+@app.get('/account')
+def list_all_movies():
+    return render_template('account.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
