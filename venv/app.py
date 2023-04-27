17713a477576f07b7422b1219285b8edc7a26d14
Eli Huffman <113002152+wolfyman411@users.noreply.github.com>
2023-04-27 13:16:22 -0400

    I fixed

--- venv/app.py
+++ venv/app.py
@@ -4,12 +4,15 @@ from src.repositories.post_repository import post_repository_singleton
 import pymysql
 import random
 
+from sqlalchemy import func
+from src.models import Post
+
 from src.models import db
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-'mysql+pymysql://root:MyLiyu2319*@localhost:3306/Reddit'
+'mysql+pymysql://root:password@localhost:3306/Reddit'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -28,13 +31,30 @@ def test():
     return render_template('test.html', posts=posts)
 
 
+"""@app.route('/createpost', methods=['POST', 'GET'])
+def postcreate():
+    if request.method == 'POST':
+        Post_Id = 3
+        C_Name = request.form.get('post_name', "hi")
+        User_Id = 0
+        Words = request.form.get('post_content', "hello")
+        print("C_Name: ", C_Name)
+        print("Words: ", Words)
+        return render_template('createpost.html', C_Name=C_Name, Words=Words)
+    return render_template('createpost.html')"""
+
 @app.route('/createpost', methods=['POST', 'GET'])
 def postcreate():
-    Post_Id = 3
-    C_Name = request.form.get('post_name', "hi")
-    User_Id = 0
-    Words = request.form.get('post_content', "hello")
-    created_post = post_repository_singleton.create_post(Post_Id, C_Name, 0, Words)
+    C_Name = ""
+    Words = ""
+    if request.method == 'POST':
+        C_Name = request.form.get('post_name')
+        Words = request.form.get('post_content')
+        Post_Id = db.session.query(func.count(Post.Post_Id)+1).scalar()
+        User_Id = 0
+        if (C_Name != ""):
+            created_post = post_repository_singleton.create_post(Post_Id, C_Name, User_Id, Words)
+            return redirect('/home')
     return render_template('createpost.html')
 
 
